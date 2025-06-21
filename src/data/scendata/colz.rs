3b643b0d028d3930fff2db9394dc26b42ccf1b38
SpeckyYT <specky.fans@gmail.com>
2025-06-21 22:01:16 +0200

    wrote macro for collision drawing

--- src/data/scendata/colz.rs
+++ src/data/scendata/colz.rs
@@ -91,6 +91,7 @@ impl ScenSegment for CollisionData {
     }
 }
 
+#[inline]
 fn draw_collision_polygon(painter: &Painter, pos_vec: Vec<Pos2>, bg_color: Color32) {
     let shap = Shape::convex_polygon(pos_vec, bg_color, Stroke::new(1.0, COLLISION_OUTLINE_COLOR));
     painter.add(shap);
@@ -99,65 +100,77 @@ fn draw_collision_polygon(painter: &Painter, pos_vec: Vec<Pos2>, bg_color: Color
 pub fn draw_collision(painter: &Painter, rect: &Rect, col_type: u8) {
     puffin::profile_function!();
 
-    let lt = rect.left_top();
-    let rt = rect.right_top();
-    let rb = rect.right_bottom();
-    let lb = rect.left_bottom();
-    let rc = rect.right_center();
-    let lc = rect.left_center();
-    let ct = rect.center_top();
-    let cb = rect.center_bottom();
-
-    match col_type {
+    macro_rules! colz {
+        ($($byte:tt $(($test:ident))? => $content:tt),*) => {
+            match col_type {
+                $(
+                    $byte => colz!(# $content),
+                )*
+            }
+        };
+        (# ($vec:tt, $color:expr)) => { draw_collision_polygon(painter, colz!(@ $vec).to_vec(), $color) };
+        (# { $($content:tt)* }) => {{ $($content)* }};
+        (@ [$($pos:tt),*]) => { [$(colz!(@ $pos)),*] };
+        (@ lt) => { rect.left_top() };
+        (@ rt) => { rect.right_top() };
+        (@ rb) => { rect.right_bottom() };
+        (@ lb) => { rect.left_bottom() };
+        (@ rc) => { rect.right_center() };
+        (@ lc) => { rect.left_center() };
+        (@ ct) => { rect.center_top() };
+        (@ cb) => { rect.center_bottom() };
+        (@ $pos:expr) => { $pos };
+    }
+    colz!{
         0x00 => { /* Blank */ },
-        0x01 => draw_collision_polygon(painter, vec![lt,rt,rb,lb],COLLISION_BG_COLOR),
-        0x02 => draw_collision_polygon(painter, vec![lt,rt,rb,lb],COLLISION_BG_COLOR_PASSABLE),
-        0x03 => draw_collision_polygon(painter, vec![lb,rc,rb],COLLISION_BG_COLOR),
-        0x04 => draw_collision_polygon(painter, vec![lc,rt,rb,lb],COLLISION_BG_COLOR),
-        0x05 => draw_collision_polygon(painter, vec![lb,ct,rt,rb],COLLISION_BG_COLOR),
-        0x06 => draw_collision_polygon(painter, vec![rt,rb,cb],COLLISION_BG_COLOR),
-        0x07 => draw_collision_polygon(painter, vec![lb,rt,rb],COLLISION_BG_COLOR),
-        0x09 => draw_collision_polygon(painter, vec![lt,rt,rb,lb], COLLISION_BG_COLOR_LAVA),
-        0x12 => draw_collision_polygon(painter, vec![lt,rt,rb,lb], COLLISION_BG_COLOR_WATER_STILL),
-        0x14 => draw_collision_polygon(painter, vec![lb,rc,rb], COLLISION_BG_COLOR_PASSABLE),
-        0x15 => draw_collision_polygon(painter, vec![lc,rt,rb,lb], COLLISION_BG_COLOR_PASSABLE),
-        0x16 => draw_collision_polygon(painter, vec![lb,ct,rt,rb], COLLISION_BG_COLOR_PASSABLE),
-        0x17 => draw_collision_polygon(painter, vec![cb,rt,rb], COLLISION_BG_COLOR_PASSABLE),
-        0x18 => draw_collision_polygon(painter, vec![lb,rt,rb], COLLISION_BG_COLOR_PASSABLE),
+        0x01 => ([lt,rt,rb,lb], COLLISION_BG_COLOR),
+        0x02 => ([lt,rt,rb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x03 => ([lb,rc,rb], COLLISION_BG_COLOR),
+        0x04 => ([lc,rt,rb,lb], COLLISION_BG_COLOR),
+        0x05 => ([lb,ct,rt,rb], COLLISION_BG_COLOR),
+        0x06 => ([rt,rb,cb], COLLISION_BG_COLOR),
+        0x07 => ([lb,rt,rb], COLLISION_BG_COLOR),
+        0x09 => ([lt,rt,rb,lb], COLLISION_BG_COLOR_LAVA),
+        0x12 => ([lt,rt,rb,lb], COLLISION_BG_COLOR_WATER_STILL),
+        0x14 => ([lb,rc,rb], COLLISION_BG_COLOR_PASSABLE),
+        0x15 => ([lc,rt,rb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x16 => ([lb,ct,rt,rb], COLLISION_BG_COLOR_PASSABLE),
+        0x17 => ([cb,rt,rb], COLLISION_BG_COLOR_PASSABLE),
+        0x18 => ([lb,rt,rb], COLLISION_BG_COLOR_PASSABLE),
         0x1A => { /* Coin */ },
-        0x1B => draw_collision_polygon(painter, vec![lt,rt,rb,lb], COLLISION_BG_COLOR_SOFT_ROCK),
-        0x1F => draw_collision_polygon(painter, vec![lb,rt,rb], COLLISION_BG_COLOR_PASSABLE),
-        0x43 => draw_collision_polygon(painter, vec![lc,rb,lb],COLLISION_BG_COLOR),
-        0x44 => draw_collision_polygon(painter, vec![lt,rc,rb,lb],COLLISION_BG_COLOR),
-        0x45 => draw_collision_polygon(painter, vec![lt,ct,rb,lb],COLLISION_BG_COLOR),
-        0x46 => draw_collision_polygon(painter, vec![lt,cb,lb],COLLISION_BG_COLOR),
-        0x47 => draw_collision_polygon(painter, vec![lt,rb,lb],COLLISION_BG_COLOR),
-        0x54 => draw_collision_polygon(painter, vec![lc,rb,lb], COLLISION_BG_COLOR_PASSABLE),
-        0x55 => draw_collision_polygon(painter, vec![lt,rc,rb,lb], COLLISION_BG_COLOR_PASSABLE),
-        0x56 => draw_collision_polygon(painter, vec![lt,ct,rb,lb], COLLISION_BG_COLOR_PASSABLE),
-        0x57 => draw_collision_polygon(painter, vec![lt,cb,lb], COLLISION_BG_COLOR_PASSABLE),
-        0x58 => draw_collision_polygon(painter, vec![lt,rb,lb], COLLISION_BG_COLOR_PASSABLE),
-        0x83 => draw_collision_polygon(painter, vec![lt,rt,rc], COLLISION_BG_COLOR),
-        0x84 => draw_collision_polygon(painter, vec![lt,rt,rb,lc], COLLISION_BG_COLOR),
-        0x85 => draw_collision_polygon(painter, vec![lt,rt,rb,cb], COLLISION_BG_COLOR),
-        0x86 => draw_collision_polygon(painter, vec![ct,rt,rb], COLLISION_BG_COLOR),
-        0x87 => draw_collision_polygon(painter, vec![lt,rt,rb],COLLISION_BG_COLOR),
-        0xC3 => draw_collision_polygon(painter, vec![lt,rt,lc],COLLISION_BG_COLOR),
-        0xC4 => draw_collision_polygon(painter, vec![lt,rt,rc,lb],COLLISION_BG_COLOR),
-        0xC5 => draw_collision_polygon(painter, vec![lt,rt,cb,lb], COLLISION_BG_COLOR),
-        0xC6 => draw_collision_polygon(painter, vec![lt,ct,lb], COLLISION_BG_COLOR),
-        0xC7 => draw_collision_polygon(painter, vec![lt,rt,lb],COLLISION_BG_COLOR),
+        0x1B => ([lt,rt,rb,lb], COLLISION_BG_COLOR_SOFT_ROCK),
+        0x1F => ([lb,rt,rb], COLLISION_BG_COLOR_PASSABLE),
+        0x43 => ([lc,rb,lb], COLLISION_BG_COLOR),
+        0x44 => ([lt,rc,rb,lb], COLLISION_BG_COLOR),
+        0x45 => ([lt,ct,rb,lb], COLLISION_BG_COLOR),
+        0x46 => ([lt,cb,lb], COLLISION_BG_COLOR),
+        0x47 => ([lt,rb,lb], COLLISION_BG_COLOR),
+        0x54 => ([lc,rb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x55 => ([lt,rc,rb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x56 => ([lt,ct,rb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x57 => ([lt,cb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x58 => ([lt,rb,lb], COLLISION_BG_COLOR_PASSABLE),
+        0x83 => ([lt,rt,rc], COLLISION_BG_COLOR),
+        0x84 => ([lt,rt,rb,lc], COLLISION_BG_COLOR),
+        0x85 => ([lt,rt,rb,cb], COLLISION_BG_COLOR),
+        0x86 => ([ct,rt,rb], COLLISION_BG_COLOR),
+        0x87 => ([lt,rt,rb], COLLISION_BG_COLOR),
+        0xC3 => ([lt,rt,lc], COLLISION_BG_COLOR),
+        0xC4 => ([lt,rt,rc,lb], COLLISION_BG_COLOR),
+        0xC5 => ([lt,rt,cb,lb], COLLISION_BG_COLOR),
+        0xC6 => ([lt,ct,lb], COLLISION_BG_COLOR),
+        0xC7 => ([lt,rt,lb], COLLISION_BG_COLOR),
         _ => {
             // Unknown, put text
             painter.rect_filled(*rect, 0.0, COLLISION_BG_COLOR);
             painter.text(
-                lt + Vec2::new(1.0, 1.0), Align2::LEFT_TOP,
+                rect.left_top() + Vec2::new(1.0, 1.0), Align2::LEFT_TOP,
                 format!("{:02X}",col_type),
                 FontId { size: 12.0, family: egui::FontFamily::Monospace },
                 Color32::BLACK
             );
             painter.text(
-                lt, Align2::LEFT_TOP,
+                rect.left_top(), Align2::LEFT_TOP,
                 format!("{:02X}",col_type),
                 FontId { size: 12.0, family: egui::FontFamily::Monospace },
                 Color32::WHITE
